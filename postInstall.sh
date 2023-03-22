771c4f887c497a5d0f6f2d0c84132633a59b72db
Sabir Khan <sabirkhanek66@gmail.com>
2023-03-23 03:16:07 +0500

    fixed another issue

--- postInstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-    pip install numpy pandas joblib scipy scikit-learn nltk
-    cd node_modules/whatsapp-web.js/src/util
-    sed -i "s/window.Store.MediaPrep = window.mR.findModule('MediaPrep')[0];/window.Store.MediaPrep = window.mR.findModule('prepRawMedia')[0];/g" Injected.js
